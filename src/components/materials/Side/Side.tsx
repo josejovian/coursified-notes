22fdf7cda42ecf67625bd6a17209205db8800f82
Jose Jovian <josejovian55@gmail.com>
2022-11-22 20:14:19 +0700

    feat: sidebar (wip)

--- src/components/materials/Side/Side.tsx
+++ src/components/materials/Side/Side.tsx
@@ -1,35 +1,57 @@
 import { useMemo } from "react";
 import { CourseType } from "@/src/type";
 import clsx from "clsx";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 interface SideProps {
 	courseDetail: CourseType;
 }
 
 export function Side({ courseDetail }: SideProps) {
-	const { title, sections } = courseDetail;
+	const { id, title, sections, description } = courseDetail;
 
 	const renderSections = useMemo(
 		() =>
-			sections.map(({ id, title, chapters }) => {
+			sections.map((section, idx1) => {
+				const _id = section.id;
+				const _title = section.title;
+				const { chapters } = section;
+
+				idx1++;
+
 				const sectionId = `Side_section-${id}`;
 
 				return (
 					<>
-						<div key={sectionId} className="CourseMaterial_entry">
-							{title}
+						<div
+							key={sectionId}
+							className="CourseMaterial_header CourseMaterial_entry"
+						>
+							<div className="CourseMaterial_index">{idx1}</div>
+							{_title}
 						</div>
-						{chapters.map((chapter) => {
-							const _id = chapter.id;
-							const _title = chapter.title;
-							const chapterId = `Side_section-${_id}-${_title}`;
+						{chapters.map((chapter, idx2) => {
+							idx2++;
+
+							const __id = chapter.id;
+							const __title = chapter.title;
+							const chapterId = `Side_section-${__id}-${__title}`;
 							return (
-								<div
+								<Link
+									href={`/course/${id}/${_id}/${__id}`}
 									key={chapterId}
-									className="CourseMaterial_entry"
+									passHref
 								>
-									{_title}
-								</div>
+									<a>
+										<div className="CourseMaterial_entry">
+											<div className="CourseMaterial_index">
+												{idx1}.{idx2}
+											</div>
+											{__title}
+										</div>
+									</a>
+								</Link>
 							);
 						})}
 					</>
@@ -42,6 +64,7 @@ export function Side({ courseDetail }: SideProps) {
 		<aside id="CourseMaterial_side" className="shadow-lg">
 			<div className="p-8">
 				<h2>{title}</h2>
+				<p>{description}</p>
 			</div>
 			<hr />
 			{renderSections}
